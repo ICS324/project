f9679ba40e3cf0639a787f1950ddecdf12733a99
ibra5him <ibra5him@hotmail.com>
2015-08-05 15:40:37 +0300

    .

--- Phase II/DatabaseApplication/src/databaseapplication/instructor/Main.java
+++ Phase II/DatabaseApplication/src/databaseapplication/instructor/Main.java
@@ -1,11 +1,28 @@
 package databaseapplication.instructor;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		new InstructorMainWindow();
+		JFrame f = new JFrame();
+		JButton b = new JButton("");
+		b.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new InstructorMainWindow();
+				
+			}
+		});
 		
+		f.add(b);
+		f.setVisible(true);
 	}
 
 }
